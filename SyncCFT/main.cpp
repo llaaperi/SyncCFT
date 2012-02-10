cf5db5f848c824a69f8c659923f6ef0765acd891
Lauri Laaperi <lauri.laaperi@gmail.com>
2012-02-10 21:43:23 +0200

    Initial Commit

---
+++ SyncCFT/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SyncCFT
+//
+//  Created by Lauri Lääperi on 10.2.2012.
+//  Copyright (c) 2012 student. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
