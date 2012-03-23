81887c12941b9c1025882e578e1339260572552f
Matias Elo <matias.elo@vtt.fi>
2012-03-23 16:34:43 +0200

    Added SessionHandler class template.

---
+++ SyncCFT/SessionHandler.cc
@@ -0,0 +1,12 @@
+//
+//  SessionHandler.cc
+//  SyncCFT
+//
+//  Created by Elo Matias on 3/23/12.
+//  Copyright (c) 2012 student. All rights reserved.
+//
+
+#include <iostream>
+
+#include "SessionHandler.hh"
+
---
+++ SyncCFT/SessionHandler.hh
@@ -0,0 +1,27 @@
+//
+//  SessionHandler.hh
+//  SyncCFT
+//
+//  Created by Elo Matias on 3/23/12.
+//  Copyright (c) 2012 student. All rights reserved.
+//
+
+#ifndef SyncCFT_SessionHandler_hh
+#define SyncCFT_SessionHandler_hh
+
+class SessionHandler {
+    SessionHandler(){};
+    ~SessionHandler(){};
+    
+    
+    
+public:
+    
+    
+private:
+    // Rule of three
+    SessionHandler(SessionHandler const& other);
+    SessionHandler& operator=(SessionHandler const& other);
+};
+
+#endif
