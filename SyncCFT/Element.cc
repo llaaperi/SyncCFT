750c25d0e866208e8b34083da697f9ae5335de53
Matias Elo <matias.elo@gmail.com>
2012-03-30 16:06:31 +0300

    Element class separated to own file. Copy constructor added.

---
+++ SyncCFT/Element.cc
@@ -0,0 +1,9 @@
+//
+//  Element.cc
+//  SyncCFT
+//
+//  Created by Elo Matias on 30.3.2012.
+//  Copyright (c) 2012 student. All rights reserved.
+//
+
+#include <iostream>
---
+++ SyncCFT/Element.hh
@@ -0,0 +1,14 @@
+//
+//  Element.hh
+//  SyncCFT
+//
+//  Created by Elo Matias on 30.3.2012.
+//  Copyright (c) 2012 student. All rights reserved.
+//
+
+#ifndef SyncCFT_Element_hh
+#define SyncCFT_Element_hh
+
+
+
+#endif
