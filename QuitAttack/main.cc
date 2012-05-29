849d98df4b4962bb3e44862731ee04d562cf6d29
Matias Elo <matias.elo@gmail.com>
2012-05-29 22:21:03 +0300

    QuitAttack tool. First draft, sends only UDP packets.

---
+++ QuitAttack/main.cc
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  QuitAttack
+//
+//  Created by Elo Matias on 29.5.2012.
+//  Copyright (c) 2012 VTT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
